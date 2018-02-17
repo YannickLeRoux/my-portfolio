8c32f66dba1216960aa40dcaca17bc2ab029f707
YannickLeRoux <rasyann@hotmail.com>
2018-02-17 15:10:45 -0500

    added static url for javascript

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config ('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','yannick-dev.com']
+ALLOWED_HOSTS = ['127.0.0.1','159.65.188.166','yannick-dev.com']
 
 # Application definition
 
