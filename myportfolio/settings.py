a6ad1fab0837149eb904542f578ac1675df4e22a
YannickLeRoux <rasyann@hotmail.com>
2018-02-18 15:16:25 -0500

    New Host added

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config ('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','159.65.188.166','yannick-dev.com']
+ALLOWED_HOSTS = ['165.227.212.4','yannick-dev.com']
 
 # Application definition
 
