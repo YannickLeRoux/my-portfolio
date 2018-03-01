d29bc7d990a147f7d431287d8d4cd69945dff8f0
YannickLeRoux <rasyann@hotmail.com>
2018-02-28 22:33:28 -0500

    Copied admin css files and added markdown for blog

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.utils import timezone
+from django.utils.html import mark_safe
+from django.utils.text import Truncator
+from markdown import markdown
 
 class Post(models.Model):
     author = models.CharField(max_length=100)
@@ -10,6 +13,10 @@ class Post(models.Model):
     published_date = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
-        return self.title
+        truncated_message = Truncator(self.text)
+        return truncated_message.chars(200)
+    
+    def get_message_as_markdown(self):
+        return mark_safe(markdown(self.text, safe_mode='escape'))
 
 # Create your models here.
--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -24,7 +24,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = config ('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
