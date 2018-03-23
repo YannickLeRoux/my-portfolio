ad70ccdd3a46a5652c885338c60aec360377d620
YannickLeRoux <rasyann@hotmail.com>
2018-03-23 00:37:34 -0400

    Improvments here and there

--- portfolio/forms.py
+++ portfolio/forms.py
@@ -5,7 +5,7 @@ class ContactForm(forms.Form):
     from_email = forms.EmailField(required=True)
     message = forms.CharField(
         widget=forms.Textarea(
-            attrs={'rows': 5, 'placeholder': 'What is on your mind?'}
+            attrs={'rows': 5, 'placeholder': 'Looking for a junior developer? Need a cool website? Let\'s get in touch!'}
         ),
         max_length=4000,
         help_text='The max length of the text is 4000.'
