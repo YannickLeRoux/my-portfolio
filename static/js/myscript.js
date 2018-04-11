36039b5a87c1d777c87446a834f2c862072e368d
YannickLeRoux <rasyann@hotmail.com>
2018-04-11 12:36:06 -0400

    laptop animation index page

--- static/js/myscript.js
+++ static/js/myscript.js
@@ -1,5 +1,24 @@
 const images = document.querySelectorAll('.image-portfolio');
 
+const laptop = document.querySelector('#laptop-index');
+const jumbo = document.querySelector('.jumbotron');
+
+function debounce(func, wait = 20, immediate = true) {
+    var timeout;
+    return function() {
+      var context = this, args = arguments;
+      var later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  };
+
+
 function handleImgOver() {
     const img = this.querySelector('img');
     const overlay = this.querySelector('.overlay');
@@ -18,6 +37,16 @@ function handleImgLeave() {
 }
 
 
-
 images.forEach(image => image.addEventListener('mouseover', handleImgOver));
 images.forEach(image => image.addEventListener('mouseleave', handleImgLeave));
+
+
+function handleShowLaptopIcon() {
+    const triggerScroll = (window.scrollY + window.innerHeight) - jumbo.offsetHeight;
+    if (triggerScroll > 290) {
+        laptop.classList.remove('laptop-hide');
+    }
+}
+
+
+window.addEventListener('scroll', debounce(handleShowLaptopIcon));
